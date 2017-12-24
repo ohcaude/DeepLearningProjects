b078d027758aac702c0f868143afbc1777d5791f
Oriol <oriol@Oriols-MacBook-Pro.local>
2017-12-24 03:37:53 -0600

    fix bug on download_data.py

--- icebergs/download_data.py
+++ icebergs/download_data.py
@@ -17,8 +17,8 @@ fname='train.json'
 if not os.path.isfile('data/'+fname):
     print('Downloading ' + fname)
     subprocess.run(['kg', 'download', '-u', username, '-p', password ,'-c', competition,'-f',fname+'.7z'])
-    subprocess.run(['7z', 'e', fname+'.7z', '-o./data/'])+fname
-    subprocess.run(['rm', fname+'.7z')
+    subprocess.run(['7z', 'e', fname+'.7z', '-o./data/'])
+    subprocess.run(['rm', fname+'.7z')]
 else:
     print(fname + ' was already downloaded')
 
