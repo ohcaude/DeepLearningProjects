64900e30ca01e0888cb59b8b637a09931436b473
Oriol Caudevilla <oriolcaudevilla@as5140.tbc.iit.edu>
2017-12-26 15:23:40 -0600

    delete a line

--- icebergs/cnn_model.py
+++ icebergs/cnn_model.py
@@ -93,6 +93,7 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
+
 # summarize history for loss
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
