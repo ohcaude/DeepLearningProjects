53317cce31e07858bc87b74e5a6a685ec90b6511
Oriol Caudevilla <oriolcaudevilla@as5140.tbc.iit.edu>
2018-02-10 21:22:25 -0600

    updates

--- icebergs/cnn_model.py
+++ icebergs/cnn_model.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from keras.models import load_model,Model,Sequential
 from keras.callbacks import ModelCheckpoint
 
-from denoising import load_denoised_data
+from data import load_data
 
 new_model = True
 
@@ -26,7 +26,7 @@ new_model = True
 #X = (X-meanX)/stdX
 #Y = df.is_iceberg.values
 
-X,Y = load_denoised_data()
+X,Y = load_data(False)
 
 # define parameters
 num_classes=1
@@ -35,7 +35,7 @@ input_shape = X.shape[1:]
 
 if new_model:
     model = Sequential()
-    model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1),activation='relu',input_shape=input_shape))
+    model.add(Conv2D(128, kernel_size=(3, 3), strides=(1, 1),activation='relu',input_shape=input_shape))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
@@ -60,24 +60,14 @@ if new_model:
     model.add(Dropout(0.3))
     model.add(Dense(num_classes, activation='sigmoid'))
 else:
-    old_model = load_model('icebergs/models/my_model_kaggle4.h5')
-    intermediate_model = Model(inputs=old_model.input,outputs=old_model.get_layer(index=12).output)
-    model=Sequential()
-    for layer in intermediate_model.layers:
-        layer.trainable=False
-        model.add(layer)
-    model.add(Dense(1024,activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(512,activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(num_classes,activation='sigmoid'))
+    model = load_model('icebergs/models/denoised2.h5')
 
 # data augmentation
 datagen = ImageDataGenerator(featurewise_center=False,featurewise_std_normalization=False,rotation_range=180,width_shift_range=0.2,height_shift_range=0.2,horizontal_flip=True,vertical_flip=True)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 datagen.fit(X_train)
 
-valgen = ImageDataGenerator(featurewise_center=True,featurewise_std_normalization=True)
+valgen = ImageDataGenerator(featurewise_center=False,featurewise_std_normalization=False)
 valgen.fit(X_test)
 
 # train/validate
@@ -86,9 +76,9 @@ checkpointer = ModelCheckpoint(filepath='/tmp/weights.hdf5', verbose=1, save_bes
 if new_model:
     history = model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size), steps_per_epoch=int(len(X_train)/batch_size), epochs=1000,validation_data=valgen.flow(X_test,Y_test), verbose=1,callbacks=[checkpointer])
 else:
-    history = model.fit_generator(datagen.flow(X_train,Y_train,batch_size=batch_size), steps_per_epoch = int(len(X_train)/batch_size),epochs=300,validation_data=valgen.flow(X_test,Y_test),verbose=1,initial_epoch=150)
+    history = model.fit_generator(datagen.flow(X_train,Y_train,batch_size=batch_size), steps_per_epoch = int(len(X_train)/batch_size),epochs=2000,validation_data=valgen.flow(X_test,Y_test),verbose=1,initial_epoch=1000,callbacks=[checkpointer])
 
-model.save('icebergs/models/denoised.h5')
+model.save('icebergs/models/new_model.h5')
 #pickle.dump((meanX,stdX),open('icebergs/models/normalization_kaggle7.pckl','wb'))
 
 #show results
--- icebergs/predict_submission.py
+++ icebergs/predict_submission.py
@@ -3,31 +3,32 @@ import ujson
 import numpy as np
 from keras.models import load_model
 import subprocess
+from denoising import load_denoised_data
+#filename = './data/test.json'
+#with open(filename,'r') as f:
+#    data = ujson.loads(f.read())
 
-filename = './data/test.json'
-with open(filename,'r') as f:
-    data = ujson.loads(f.read())
+#print('data loaded')
 
-print('data loaded')
+#ids = [d["id"] for d in data]
+#band1 = np.array([d["band_1"] for d in data])
+#band2 = np.array([d["band_2"] for d in data])
+#band3 = band1/band2
+#del data
 
-ids = [d["id"] for d in data]
-band1 = np.array([d["band_1"] for d in data])
-band2 = np.array([d["band_2"] for d in data])
-band3 = band1/band2
-del data
+#X = np.stack((np.reshape(band1,(band1.shape[0],75,75)),np.reshape(band2,(band2.shape[0],75,75)),np.reshape(band3,(band3.shape[0],75,75))),axis=-1)
+#del band1
+#del band2
+X,ids = load_denoised_data(True)
 
-X = np.stack((np.reshape(band1,(band1.shape[0],75,75)),np.reshape(band2,(band2.shape[0],75,75)),np.reshape(band3,(band3.shape[0],75,75))),axis=-1)
-del band1
-del band2
+model = load_model('icebergs/models/denoised3.h5')
+#meanX,stdX = pickle.load(open('models/normalization_kaggle8.pckl','rb'))
 
-model = load_model('models/my_model_kaggle8.h5')
-meanX,stdX = pickle.load(open('models/normalization_kaggle8.pckl','rb'))
-
-X = (X-meanX) / stdX
+#X = (X-meanX) / stdX
 
 y_pred = model.predict(X,verbose=1)
 
-with open('data/submit8.txt','a') as myfile:
+with open('icebergs/data/submit11.txt','a') as myfile:
     myfile.write("id,is_iceberg\n")
     for idval,prediction in zip(ids,y_pred):
         myfile.write(str(idval)+','+str(prediction[0])+'\n')
@@ -36,4 +37,4 @@ fname='test.json'
 username='ocaudevi'
 password='eO7MYdZ4Kni5'
 competition='statoil-iceberg-classifier-challenge'
-subprocess.run(['kg', 'submit','data/submit8.txt', '-u', username, '-p', password ,'-c', competition])
+subprocess.run(['kg', 'submit','icebergs/data/submit11.txt', '-u', username, '-p', password ,'-c', competition])
